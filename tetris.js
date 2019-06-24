a525ee39e3eb3bcdd1c455100e2e7f18442c633f
aimbotboaz <48298717+aimbotboaz@users.noreply.github.com>
2019-06-24 14:02:10 +0200

    Add files via upload

--- tetris.js
+++ tetris.js
@@ -86,7 +86,7 @@ Piece.prototype.unDraw = function(){
     this.fill(VACANT);
 }
 
-
+//functions 
 
 Piece.prototype.moveDown = function(){
     if(!this.collision(0,1,this.activeTetromino)){
@@ -209,20 +209,17 @@ Piece.prototype.collision = function(x,y,piece){
     }
     return false;
 }
-
+//controls
 
 document.addEventListener("keydown",CONTROL);
 
 function CONTROL(event){
     if(event.keyCode == 37){
         p.moveLeft();
-        //dropStart = Date.now();
     }else if(event.keyCode == 38){
         p.rotate();
-        //dropStart = Date.now();
     }else if(event.keyCode == 39){
         p.moveRight();
-        //dropStart = Date.now();
     }else if(event.keyCode == 40){
         p.moveDown();
     }
